eecef4a440583300c1961603308e74e5be082318
jityoink <79625497+jityoink@users.noreply.github.com>
2023-02-09 13:22:39 +0800

    yay

--- notebooks/caesar.py
+++ notebooks/caesar.py
@@ -5,20 +5,16 @@ def caesar():
         key = input("input key: ")
         try:
             key = int(key)
+            ciphertext = ""
             for x in range(len(plaintext)):
-                if ord(plaintext[x]) not in [*range(65,91),*range(97,123)]:
-                    check = check + 1
-            if check == 0:
-                ciphertext = ""
-                for x in range(len(plaintext)):
-                    if ord(plaintext[x]) in [*range(65,91)]:
-                        ciphertext += chr(64 + (ord(plaintext[x]) - 64 + key)%26)
-                    elif ord(plaintext[x]) in [*range(97,123)]:
-                        ciphertext += chr(97 + (ord(plaintext[x]) - 97 + key)%26)
-                print(ciphertext)
-                break
-            else:
-                print("use only uppercase/lowercase")
+                if ord(plaintext[x]) in [*range(65,91)]:
+                    ciphertext += chr(64 + (ord(plaintext[x]) - 64 + key)%26)
+                elif ord(plaintext[x]) in [*range(97,123)]:
+                    ciphertext += chr(97 + (ord(plaintext[x]) - 97 + key)%26)
+                else:
+                    ciphertext += plaintext[x]
+            print(ciphertext)
+            break
         except ValueError:
             print("enter a number for the key")
 
---
+++ notebooks/substitution.py
@@ -0,0 +1,34 @@
+def substitution():
+    while True:
+        plaintext = input("plaintext: ")
+        key = input("key: ")
+        check = 0
+        cipher = set({})
+        for x in range(0,len(key)):
+            cipher.add(key[x])
+        if len(cipher) != 26 or ord(key[x]) not in [*range(65,91),*range(97,123)]:
+            check = check + 1
+        if len(key) == 26 and check == 0:
+            keylookup = {
+
+            }
+            ascii = [*range(65,91),*range(97,123)]
+            a_list = []
+            for x in range(52):
+                a_list += chr(ascii[x])
+            for x in range(0,26):
+                keylookup.update({a_list[x]:key[x].upper()})
+            for x in range(26, 52):
+                keylookup.update({a_list[x]:key[x-26].lower()})
+            ciphertext = ""
+            for x in range(len(plaintext)):
+                if ord(plaintext[x]) in [*range(65,91),*range(97,123)]:
+                    ciphertext += keylookup[plaintext[x]]
+                else:
+                    ciphertext += plaintext[x]
+            print(f"ciphertext: {ciphertext}")
+            break
+        else:
+            print("enter 26 unique letters in the key")
+
+substitution()
